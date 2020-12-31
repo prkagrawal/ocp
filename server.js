79b5749acd728fab1fdaa3fd1cda7612681411b1
Prince Agrawal <princeagrawalnew123@gmail.com>
2020-12-31 13:16:33 +0530

    setup backend

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to ocp api')
+});
+
+app.get('/api/products', (req,res) => {
+    res.send(data.products);
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
