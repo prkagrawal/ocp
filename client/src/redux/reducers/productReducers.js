2328491a57be0088cc848e78cd97630170ad84a0
Prince Agrawal <princeagrawalnew123@gmail.com>
2021-01-01 21:26:58 +0530

    added redux to Products screen

--- client/src/redux/reducers/productReducers.js
+++ client/src/redux/reducers/productReducers.js
@@ -2,14 +2,15 @@ import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
     PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
 } from '../constants/productConstants';
 
-const initialState = {
-    loading: true,
-    products: []
-}
-
-export const productListReducer = (state = initialState, action) => {
+export const productListReducer = (
+    state = { loading: true, products: [] },
+    action
+) => {
     switch (action.type) {
         case PRODUCT_LIST_REQUEST:
             return {
@@ -25,6 +26,22 @@ export const productListReducer = (state = initialState, action) => {
                 loading: false,
                 error: action.payload
             };
+        default:
+            return state;
+    }
+};
+
+export const productDetailsReducer = (
+    state = { product: {}, loading: true },
+    action
+) => {
+    switch (action.type) {
+        case PRODUCT_DETAILS_REQUEST:
+            return { loading: true };
+        case PRODUCT_DETAILS_SUCCESS:
+            return { loading: false, product: action.payload };
+        case PRODUCT_DETAILS_FAIL:
+            return { loading: false, error: action.payload };
         default:
             return state;
     }
--- server.js
+++ server.js
@@ -11,6 +11,15 @@ app.get('/', (req, res) => {
     res.send('Welcome to ocp api')
 });
 
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((x) => x._id === req.params.id);
+    if (product) {
+        res.send(product);
+    } else {
+        res.status(404).send({ message: 'Product Not Found' });
+    }
+})
+
 app.get('/api/products', (req,res) => {
     res.send(data.products);
 });
