680ba0d57d037cb1a41983e73b2c500c4952ece9
Prince Agrawal <princeagrawalnew123@gmail.com>
2021-01-01 20:51:47 +0530

    added Redux to home screen

---
+++ client/src/redux/helpers/store.js
@@ -0,0 +1,20 @@
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+import { productListReducer } from '../reducers/productReducers';
+
+const initialState = {};
+
+const reducer = combineReducers({
+    productList: productListReducer
+});
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
+
+const store = createStore(
+    reducer,
+    initialState,
+    composeEnhancer(applyMiddleware(thunk))
+);
+
+export default store;
--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,30 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 
 import Product from '../components/Product';
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../redux/actions/productActions';
 
 export default function HomeScreen () {
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+    const dispatch = useDispatch();
+    const productList = useSelector((state) => state.productList);
+    const { loading, error, products } = productList;
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setLoading(true);
-                const req = await axios.get('http://localhost:5000/api/products');
-                const data = req.data
-                setLoading(false);
-                setProducts(data);
-            } catch (err) {
-                setError(err.message);
-                setLoading(false);
-            }
-        };
-        fetchData();
-    }, []);
+        dispatch(listProducts());
+    }, [dispatch]);
     return (
         <div>
             {loading ? (
